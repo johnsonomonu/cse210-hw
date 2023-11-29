2e705e856856d6afedd58bbd295e14cbdf443c20
johnsonomonu <johnsonomonu144@gmail.com>
2023-11-29 10:08:00 +0000

    commit

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -2,8 +2,152 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        // Create instances of each event type
+        Event lectureEvent = new Lecture("Tech Conference", "Exciting tech topics", new DateTime(2023, 12, 15), new TimeSpan(10, 30, 0),
+            new Address("123 Tech Street", "Tech City", "Tech State", "Tech Country"), "John Doe", 100);
+
+        Event receptionEvent = new Reception("Networking Mixer", "Meet and greet", new DateTime(2023, 12, 20), new TimeSpan(18, 0, 0),
+            new Address("456 Mixer Street", "Mixer City", "Mixer State", "Mixer Country"));
+        ((Reception)receptionEvent).SetRSVPInformation("info@example.com");
+
+        Event outdoorEvent = new OutdoorGathering("Summer Picnic", "Enjoy the outdoors", new DateTime(2023, 7, 8), new TimeSpan(12, 0, 0),
+            new Address("789 Park Street", "Park City", "Park State", "Park Country"));
+        ((OutdoorGathering)outdoorEvent).SetWeatherStatement("Sunny weather expected");
+
+        // Display details for each event
+        DisplayEventDetails(lectureEvent);
+        DisplayEventDetails(receptionEvent);
+        DisplayEventDetails(outdoorEvent);
+    }
+
+    static void DisplayEventDetails(Event eventObj)
+    {
+        // Display standard details
+        Console.WriteLine("Standard Details:");
+        Console.WriteLine(eventObj.GenerateStandardDetails());
+
+        // Display full details
+        Console.WriteLine("\nFull Details:");
+        Console.WriteLine(eventObj.GenerateFullDetails());
+
+        // Display short description
+        Console.WriteLine("\nShort Description:");
+        Console.WriteLine(eventObj.GenerateShortDescription());
+
+        Console.WriteLine("\n------------------------\n");
+    }
+}
+
+class Event
+{
+    private string _eventTitle;
+    private string _eventDescription;
+    private DateTime _eventDate;
+    private TimeSpan _eventTime;
+    private Address _eventAddress;
+
+    public Event(string title, string description, DateTime date, TimeSpan time, Address address)
+    {
+        _eventTitle = title;
+        _eventDescription = description;
+        _eventDate = date;
+        _eventTime = time;
+        _eventAddress = address;
+    }
+
+    public string GenerateStandardDetails()
+    {
+        return $"Title: {_eventTitle}\nDescription: {_eventDescription}\nDate: {_eventDate.ToShortDateString()}\nTime: {_eventTime}\nAddress: {_eventAddress.GetFullAddress()}";
+    }
+
+    public virtual string GenerateFullDetails()
+    {
+        return GenerateStandardDetails();
+    }
+
+    public string GenerateShortDescription()
+    {
+        return $"Type: Generic Event\nTitle: {_eventTitle}\nDate: {_eventDate.ToShortDateString()}";
+    }
+}
+
+class Lecture : Event
+{
+    private string _speakerName;
+    private int _capacity;
+
+    public Lecture(string title, string description, DateTime date, TimeSpan time, Address address, string speakerName, int capacity)
+        : base(title, description, date, time, address)
+    {
+        _speakerName = speakerName;
+        _capacity = capacity;
+    }
+
+    public override string GenerateFullDetails()
+    {
+        return $"{base.GenerateFullDetails()}\nSpeaker: {_speakerName}\nCapacity: {_capacity}";
+    }
+}
+
+class Reception : Event
+{
+    private string _rsvpEmail;
+
+    public Reception(string title, string description, DateTime date, TimeSpan time, Address address)
+        : base(title, description, date, time, address)
+    {
+    }
+
+    public void SetRSVPInformation(string email)
+    {
+        _rsvpEmail = email;
+    }
+
+    public override string GenerateFullDetails()
+    {
+        return $"{base.GenerateFullDetails()}\nRSVP Email: {_rsvpEmail}";
+    }
+}
+
+class OutdoorGathering : Event
+{
+    private string _weatherStatement;
+
+    public OutdoorGathering(string title, string description, DateTime date, TimeSpan time, Address address)
+        : base(title, description, date, time, address)
+    {
+    }
+
+    public void SetWeatherStatement(string weatherStatement)
+    {
+        _weatherStatement = weatherStatement;
+    }
+
+    public override string GenerateFullDetails()
+    {
+        return $"{base.GenerateFullDetails()}\nWeather Statement: {_weatherStatement}";
+    }
+}
+
+class Address
+{
+    private string _streetAddress;
+    private string _city;
+    private string _stateProvince;
+    private string _country;
+
+    public Address(string streetAddress, string city, string stateProvince, string country)
+    {
+        _streetAddress = streetAddress;
+        _city = city;
+        _stateProvince = stateProvince;
+        _country = country;
+    }
+
+    public string GetFullAddress()
+    {
+        return $"{_streetAddress}, {_city}, {_stateProvince}, {_country}";
     }
 }
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,154 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        // Create instances of each activity type
+        Activity runningActivity = new Running(new DateTime(2022, 11, 3), 30, 3.0);
+        Activity bicyclingActivity = new Bicycling(new DateTime(2022, 11, 3), 30, 6.0);
+        Activity swimmingActivity = new Swimming(new DateTime(2022, 11, 3), 30, 10);
+
+        // Create a list containing each activity instance
+        List<Activity> activities = new List<Activity>
+        {
+            runningActivity,
+            bicyclingActivity,
+            swimmingActivity
+        };
+
+        // Iterate through the list and call the GetSummary method for each activity
+        foreach (var activity in activities)
+        {
+            // Display the results of the GetSummary method
+            Console.WriteLine(activity.GetSummary());
+            Console.WriteLine("------------------------");
+        }
+    }
+}
+
+class Activity
+{
+    private DateTime _date;
+    protected int _lengthMinutes;
+
+    public Activity(DateTime date, int lengthMinutes)
+    {
+        _date = date;
+        _lengthMinutes = lengthMinutes;
+    }
+
+    public virtual double GetDistance()
+    {
+        return 0.0; // Default implementation (to be overridden in derived classes)
+    }
+
+    public virtual double GetSpeed()
+    {
+        return 0.0; // Default implementation (to be overridden in derived classes)
+    }
+
+    public virtual double GetPace()
+    {
+        return 0.0; // Default implementation (to be overridden in derived classes)
+    }
+
+    public virtual string GetSummary()
+    {
+        return $"{_date.ToShortDateString()} - {_lengthMinutes} min";
+    }
+}
+
+class Running : Activity
+{
+    private double _distance;
+
+    public Running(DateTime date, int lengthMinutes, double distance)
+        : base(date, lengthMinutes)
+    {
+        _distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return _distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return _distance / (_lengthMinutes / 60.0);
+    }
+
+    public override double GetPace()
+    {
+        return (_lengthMinutes / _distance);
+    }
+
+    public override string GetSummary()
+    {
+        return $"{base.GetSummary()} Running - Distance: {_distance} miles, Speed: {GetSpeed()} mph, Pace: {GetPace()} min per mile";
+    }
+}
+
+class Bicycling : Activity
+{
+    private double _speed;
+
+    public Bicycling(DateTime date, int lengthMinutes, double speed)
+        : base(date, lengthMinutes)
+    {
+        _speed = speed;
+    }
+
+    public double GetSpeedKph()
+    {
+        return _speed;
+    }
+
+    public override double GetSpeed()
+    {
+        return _speed / 0.621371; // Convert speed from kph to mph
+    }
+
+    public override double GetPace()
+    {
+        return 60.0 / (_speed / 0.621371); // Convert speed from kph to mph and calculate pace
+    }
+
+    public override string GetSummary()
+    {
+        return $"{base.GetSummary()} Bicycling - Speed: {_speed} kph, Speed: {GetSpeed()} mph, Pace: {GetPace()} min per mile";
+    }
+}
+
+class Swimming : Activity
+{
+    private int _laps;
+
+    public Swimming(DateTime date, int lengthMinutes, int laps)
+        : base(date, lengthMinutes)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return _laps * 50.0 / 1000.0; // Convert laps to distance in kilometers
+    }
+
+    public override double GetSpeed()
+    {
+        return GetDistance() / (_lengthMinutes / 60.0);
+    }
+
+    public override double GetPace()
+    {
+        return (_lengthMinutes / GetDistance());
+    }
+
+    public override string GetSummary()
+    {
+        return $"{base.GetSummary()} Swimming - Laps: {_laps}, Distance: {GetDistance()} km, Speed: {GetSpeed()} kph, Pace: {GetPace()} min per km";
     }
 }
